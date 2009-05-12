a0b224142f78d291177a9e81066df915ee88447c
vang <vang@Vang.(none)>
2009-05-12 17:12:36 -0500

    se agrego en PDFToModel  un mas

--- src/convertidores/PDFToModelo.java
+++ src/convertidores/PDFToModelo.java
@@ -25,7 +25,7 @@ import org.pdfbox.util.PDFTextStripper;
 public class PDFToModelo {
 	private FileInputStream file = null;
 	private PDDocument document = null;
-
+    private String passwordPDF = "12345red9";
     /**
      * Abre el archivo especificado en la ruta
      * @param archivo
@@ -123,8 +123,8 @@ public class PDFToModelo {
 			parser.parse();
 			document = parser.getPDDocument();
 			if (document.isEncrypted()) {
-				System.out.println("Documento encriptado..intentando desencriptar...");
-				document.decrypt("12345red9");
+				System.out.println("Documento encriptado..intentando desencriptar...");                 
+				document.decrypt("passwordPDF");
 				document.setEncryptionDictionary(null);
 				document.getDocument().getTrailer().setItem("Encrypt",null);
 			}
@@ -159,6 +159,9 @@ public class PDFToModelo {
 		} catch (FileNotFoundException e) {
 			System.out.println("El raton se callo de la rueda: no se encontro el archivo");
 			e.printStackTrace();
+		} catch (InvalidPasswordException e) {
+			System.out.println("Password Incorrecta");
+			e.printStackTrace();
 		} catch (IOException e) {
 			System.out.println("El raton se callo de la rueda: no se pudo abrir el archivo");
 			e.printStackTrace();
@@ -188,6 +191,91 @@ public class PDFToModelo {
 
 		return aPDF;
 	}
+    /*Metodo del password
+     *
+     */
+     public EntidadPDF convertirAModelo(File archivo, String passwordPDF2) throws CryptographyException, InvalidPasswordException{
+		EntidadPDF aPDF= new EntidadPDF();
+		boolean ratonSeCalloDeLaRueda=false;
+		try
+		{
+			System.out.println("Cargando el PDF...");
+			file = new FileInputStream(archivo.getAbsolutePath());
+			PDFParser parser = new PDFParser(file);
+			parser.parse();
+			document = parser.getPDDocument();
+			if (document.isEncrypted()) {
+				System.out.println("Documento encriptado..intentando desencriptar...");
+				document.decrypt(passwordPDF2);
+				document.setEncryptionDictionary(null);
+				document.getDocument().getTrailer().setItem("Encrypt",null);
+			}
+			else
+				System.out.println("Documento libre...viva la libertad!!!");
+
+			//Obtenemos las paginas
+        	PDFTextStripper stripper = new PDFTextStripper(); // no ese tipo de stripper -.-
+        	aPDF.setContenido(stripper.getText(document));
+			System.out.println("Rellenando modelo...");
+			//una ves leído le sacamos la sopa
+			PDDocumentInformation info = document.getDocumentInformation();
+	        PDDocumentCatalog cat = document.getDocumentCatalog();
+	        PDMetadata metadata = cat.getMetadata();
+
+            PDDocumentOutline outline = cat.getDocumentOutline();
+            System.out.println("Outline: "+ outline.getOpenCount());
+
+
+	        aPDF.setTitulo(info.getTitle());
+	        aPDF.setAutor(info.getAuthor());
+	        aPDF.setNumeroDePaginas(document.getNumberOfPages());
+	        aPDF.setTema(info.getSubject());
+	        aPDF.setPalabrasClave(info.getKeywords());
+	        aPDF.setCreador(info.getCreator());
+	        aPDF.setProductor(info.getProducer());
+	        aPDF.setFechaDeCreacion(info.getCreationDate());
+	        aPDF.setFechaDeModificacion(info.getModificationDate());
+	        //aPDF.setRawMetadata(metadata.toString());
+
+
+		} catch (FileNotFoundException e) {
+			System.out.println("El raton se callo de la rueda: no se encontro el archivo");
+			e.printStackTrace();
+		} catch (InvalidPasswordException e) {
+			System.out.println("Password Incorrecta");
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.out.println("El raton se callo de la rueda: no se pudo abrir el archivo");
+			e.printStackTrace();
+		}
+		finally
+		{
+			if (file != null)
+			{
+				try {
+					file.close();
+				} catch (IOException e) {
+					System.out.println("El raton se callo de la rueda: no se pudo cerrar el archivo");
+					e.printStackTrace();
+				}
+			}
+			if (document != null)
+			{
+				try {
+					document.close();
+				} catch (IOException e) {
+					System.out.println("El raton se callo de la rueda: no se pudo cerrar el documento");
+					e.printStackTrace();
+				}
+			}
+		}
+
+
+		return aPDF;
+	}
+
+
+    /********************/
 
 	public static void main(String arg[]) {
 		PDDocument document = null;
