9f4e88f0204bdea2a239a7000e7b45879d5e963a
Alos <alosii@gmail.com>
2009-05-13 14:09:17 -0500

    Added the original rev. Closes #75

--- src/convertidores/PDFToModelo.java
+++ src/convertidores/PDFToModelo.java
@@ -125,7 +125,7 @@ public class PDFToModelo {
 			if (document.isEncrypted()) {
 				System.out.println("Documento encriptado..intentando desencriptar...");
                 //probamos desencriptar con las posibles claves
-                String [] passwordPosible={"pass1","pass2","pass3"};
+                String [] passwordPosible={"12345red9","pass2","pass3"};
                 for(int i=0;i<passwordPosible.length;i++){
                     String passwordPDF = passwordPosible[i];
                     document.decrypt(passwordPDF);
